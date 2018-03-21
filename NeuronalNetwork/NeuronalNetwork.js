df327c382e576eab2a4cea02611ff04cc9b71ab9
Guamedo <gonzalopierola@gmail.com>
2018-03-21 23:03:09 +0100

    GUI for input, hidden and output.

--- NeuronalNetwork/NeuronalNetwork.js
+++ NeuronalNetwork/NeuronalNetwork.js
@@ -1,12 +1,13 @@
-// TODO
 class NeuronalNetwork{
-    constructor(input, hidden, output){
+    constructor(input, hidden, output, aFunction1 = NeuronalNetwork.reLu, aFunction2 = NeuronalNetwork.sigmoid){
         this.input = new Matrix(1, input);
         this.W1 = new Matrix(input, hidden, true);
         this.b1 = new Matrix(hidden, 1, true);
+        this.aFunction1 = aFunction1;
         this.W2 = new Matrix(hidden, output, true);
         this.b2 = new Matrix(output, 1, true);
-        this.output = new Matrix(output, 1);
+        this.aFunction2 = aFunction2;
+        this.output = new Matrix(1, output);
     }
 
     loadInputFromArray(array){
@@ -22,65 +23,84 @@ class NeuronalNetwork{
     getOutputAsArray(){
         let outputArray = [];
         for(let i = 0; i < this.output.cols; i++){
-            outputArray.push(this.output.data[0][i]);
+            outputArray.push(this.output.data[i][0]);
         }
         return outputArray;
     }
 
-    calculateOutput(){
+    predict(){
         let out = this.input.multiply(this.W1);
         console.log(out);
         out.addBias(this.b1);
+        for(let i = 0; i < out.rows; i++){
+            for(let j = 0; j < out.cols; j++){
+                out.data[i][j] = this.aFunction1(out.data[i][j]);
+            }
+        }
         out = out.multiply(this.W2);
         out.addBias(this.b2);
+        for(let i = 0; i < out.rows; i++){
+            for(let j = 0; j < out.cols; j++){
+                out.data[i][j] = this.aFunction2(out.data[i][j]);
+            }
+        }
         this.output = out;
     }
 
-    draw(){
-        let inputX = 100;
-        let hiddenX = 300;
-        let outX = 500;
+    draw(x, y, w, h){
+        let inputX = w/6;
+        let hiddenX = w/3 + w/6;
+        let outX = 2*w/3 + w/6;
 
-        let offsetI = height/this.input.cols;
-        let sizeI = Math.min(offsetI, 50);
+        let offsetI = h/this.input.cols;
+        let sizeI = Math.min(Math.ceil(offsetI), (w+h)/24);
 
-        let offsetH = height/this.b1.rows;
-        let sizeH = Math.min(offsetH, 50);
+        let offsetH = h/this.b1.rows;
+        let sizeH = Math.min(Math.ceil(offsetH), (w+h)/24);
 
-        let offsetO = height/this.output.rows;
-        let sizeO = Math.min(offsetO, 50);
+        let offsetO = h/this.output.cols;
+        let sizeO = Math.min(Math.ceil(offsetO), (w+h)/24);
 
         stroke(0);
         strokeWeight(1);
 
         for(let i = 0; i < this.W1.rows; i++){
             for(let j = 0; j < this.W1.cols; j++){
-                let patata = 5*((this.W1.data[i][j]+1)/2);
-                strokeWeight(Math.ceil(patata));
-                line(inputX, i*offsetI + offsetI/2, hiddenX, j*offsetH + offsetH/2);
+                let weight = 5*((this.W1.data[i][j]+1)/2);
+                strokeWeight(Math.ceil(weight));
+                line(inputX + x, i*offsetI + offsetI/2 + y, hiddenX + x, j*offsetH + offsetH/2 + y);
             }
         }
 
         for(let i = 0; i < this.W2.rows; i++){
             for(let j = 0; j < this.W2.cols; j++){
-                let patata = 5*((this.W2.data[i][j]+1)/2);
-                strokeWeight(Math.ceil(patata));
-                line(hiddenX, i*offsetH + offsetH/2, outX, j*offsetO + offsetO/2);
+                let weight = 5*((this.W2.data[i][j]+1)/2);
+                strokeWeight(Math.ceil(weight));
+                line(hiddenX + x, i*offsetH + offsetH/2 + y, outX + x, j*offsetO + offsetO/2 + y);
             }
         }
 
-        fill(255);
+        textAlign(CENTER, CENTER);
         strokeWeight(1);
+        textSize(Math.ceil(sizeI*0.35));
         for(let i = 0; i < this.input.cols; i++){
-            ellipse(inputX, i*offsetI + offsetI/2, sizeI, sizeI);
+            fill(0,250,154);
+            ellipse(inputX + x, i*offsetI + offsetI/2 + y, sizeI, sizeI);
+            fill(0);
+            text(this.input.data[0][i].toFixed(2), inputX + x, i*offsetI + offsetI/2 + y);
         }
 
         for(let i = 0; i < this.b1.rows; i++){
-            ellipse(hiddenX, i*offsetH + offsetH/2, sizeH, sizeH);
+            fill(255,165,0);
+            ellipse(hiddenX + x, i*offsetH + offsetH/2 + y, sizeH, sizeH);
         }
 
-        for(let i = 0; i < this.output.rows; i++){
-            ellipse(outX, i*offsetO + offsetO/2, sizeO, sizeO);
+        textSize(Math.ceil(sizeO*0.35));
+        for(let i = 0; i < this.output.cols; i++){
+            fill(0,191,255);
+            ellipse(outX + x, i*offsetO + offsetO/2 + y, sizeO, sizeO);
+            fill(0);
+            text(this.output.data[0][i].toFixed(2), outX + x, i*offsetO + offsetO/2 + y);
         }
     }
 
--- NeuronalNetwork/sketch.js
+++ NeuronalNetwork/sketch.js
@@ -1,13 +1,38 @@
 let brain;
+var pInput = 4;
+var pHidden = 3;
+var pOutput = 2;
+var input = 4;
+var hidden = 3;
+var output = 2;
 
 function setup() {
     createCanvas(600, 600);
-    background(51);
-    brain = new NeuronalNetwork(4, 3, 1);
-    brain.draw();
+    background(200);
 
+    brain = new NeuronalNetwork(input, hidden, output);
+    brain.loadInputFromArray([0.2, 0.8, 0.1, 0.67]);
+    brain.predict();
+    brain.draw(0, 0, 600, 600);
+
+    var gui = createGui('Parameters');
+    sliderRange(1, 100, 1);
+    gui.addGlobals('input', 'hidden', 'output');
 }
 
 function draw() {
-
+    if(pInput !== input || pHidden !== hidden || pOutput !== output) {
+        background(200);
+        brain = new NeuronalNetwork(input, hidden, output);
+        let inputArray = [];
+        for (let i = 0; i < input; i++) {
+            inputArray.push(random(0, 1));
+        }
+        brain.loadInputFromArray(inputArray);
+        brain.predict();
+        brain.draw(0, 0, 600, 600);
+    }
+    pInput = input;
+    pHidden = hidden;
+    pOutput = output;
 }
