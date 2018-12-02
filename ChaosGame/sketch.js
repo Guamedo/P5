017dfa5cc8aed77303fea3e196ae0a18739c433d
Guamedo <gonzalopierola@gmail.com>
2018-12-02 20:04:41 +0100

    Chaos game: refactor

--- ChaosGame/sketch.js
+++ ChaosGame/sketch.js
@@ -13,7 +13,10 @@ let img;
 let imgMask;
 
 function preload() {
+    // Load HSB circle image
     img = loadImage('images/hsb.png');
+
+    // Load mask
     imgMask = loadImage('images/hsb_mask.png');
 }
 
@@ -64,23 +67,33 @@ function setup() {
 }
 
 function draw() {
+    // Draw the points
     image(pg, 0, 0);
+
+    // Draw the HSB circle image
     push();
     image(img, width - min(width/5, height/5) - 10, 10, min(width/5, height/5), min(width/5, height/5));
     pop();
+
+    // Add 500 new points to the image and draw the lines connecting them
     for(let i = 0; i < 500 && frameCount < 2000; i++){
+        // Set the color mode to HSB
         pg.colorMode(HSB, 2*PI, min(width/2, height/2), 1, 1);
 
+        // Calculate the color for the new point
         let angle = Math.atan2(height/2 - p.y, width/2 - p.x);
         if(angle < 0){
             angle = 2*PI + angle;
         }
 
+        // Set the draw color
         pg.stroke(angle, createVector(width/2, height/2).dist(p), 1, 0.5);
+
+        // Draw the point
         pg.point(p.x, p.y);
 
+        // Calculate the next random point
         let r = floor(random(shapePointNumber));
-
         if(shapePointNumber === 5){
             switch (restrictMode) {
                 case 0:
@@ -100,28 +113,28 @@ function draw() {
                     }
                     break;
                 default:
-                    console.log("Esto no deveria ocurrir");
+                    console.log("Esto no deberia ocurrir");
                     restrictMode = 0;
                     break;
-
             }
-
-
-
         }
 
-        aux = p;
+        // Save the previous point
+        pp = p;
         p = (shapePoints[r].copy().add(p)).div(2);
 
+        // Draw the line connecting the last two points
         colorMode(HSB, 2*PI, min(width/2, height/2), 1, 1);
         stroke(angle, createVector(width/2, height/2).dist(p), 1, 1);
-        line(aux.x, aux.y, p.x, p.y);
+        line(pp.x, pp.y, p.x, p.y);
 
+        // Save the previous two reference points index
         ppVertex = pVertex;
         pVertex = r;
     }
 }
 
+// Mod with negative values
 function mod(n, m) {
     return ((n % m) + m) % m;
 }
